c61c871005880654ad5fa1bb49169027b2c3315f
saulfabreg Wii VC Project <saulfabregamboa@outlook.com>
2022-10-07 09:38:03 -0500

    SIO wait time update (fixes controls on some games) (xjsxjs197)

--- sio.c
+++ sio.c
@@ -57,12 +57,14 @@ char *Mcd2Data = (char*)MCD2_LO;
 char Mcd1Data[MCD_SIZE], Mcd2Data[MCD_SIZE];
 #endif
 
+#define SIO_CYCLES		535
+
 // clk cycle byte
 // 4us * 8bits = ((PSXCLK / 1000000) * 32) / BIAS; (linuzappz)
 #define SIO_INT() { \
 	if (!Config.Sio) { \
 		psxRegs.interrupt |= (1 << PSXINT_SIO); \
-		psxRegs.intCycle[PSXINT_SIO].cycle = 200; \
+		psxRegs.intCycle[PSXINT_SIO].cycle = SIO_CYCLES; \
 		psxRegs.intCycle[PSXINT_SIO].sCycle = psxRegs.cycle; \
 	} \
 }
